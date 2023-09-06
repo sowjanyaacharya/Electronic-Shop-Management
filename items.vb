a9de4d93a3070a9e5529ffbbbed1b25b8a78ebe9
sowjanyaacharya <136161441+sowjanyaacharya@users.noreply.github.com>
2023-09-06 17:10:57 +0530

    items.vb

--- items.vb
+++ items.vb
@@ -1,26 +1,27 @@
+'This is the window once you click on items it transfers you to this form to make some CRUD operations 
 Public Class items
-
+'here button1 to add items
 Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click 
  Additems.Show() 
 End Sub
-
+'button2 to delete items
 Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
 
  Deleteitems.Show()
 End Sub
-
+'button3 to delete items
 Private Sub Button3_Click(ByVal sender As System Object, ByVal e As System.EventArgs) Handles Button3.Click
 
  Updateitem.Show()
 
 End Sub
-
+'button4 to viewitems
 Private Sub Button4_Click(ByVal sender As System.Object, ByVal System.EventArgs) Handles Button4.Click
 
  Viewitems.Show()
 
 End Sub
-
+'button5 to close the form
 Private Sub ButtonS_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
 
  Me.Hide()
