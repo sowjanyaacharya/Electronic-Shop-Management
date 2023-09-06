c6742468a187ee4e3f07f579be86d63adb62d539
sowjanyaacharya <136161441+sowjanyaacharya@users.noreply.github.com>
2023-09-06 17:14:04 +0530

    main.vb

--- main.vb
+++ main.vb
@@ -1,19 +1,20 @@
+'Once you login you are redirected to this form where you can move to items ,stockhand, billing windows
 Public Class Form1
-
+'here buttton1 for items form
 Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
 
  Items.Show() End Sub
-
+'here buttton3 for stock in hand report form
 Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click 
  stockhand.Show()
 
 End Sub
-
+'here button2 for billing form 
 Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
 
  bill.Show()
 End Sub
-
+'here button4 for go back to previous form that is login form 
 Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
 
   Me.Hide() 
